3d09ca12cd58fbb767fd4f0ed0f7811413e7df35
Kayke Galdiano <kaykegaldiano@gmail.com>
2022-11-23 22:00:37 -0300

    modelo algébrico

--- trabalho_final.py
+++ trabalho_final.py
@@ -3,8 +3,8 @@ from math import*
 from random import*
 
 # bibliotecas para desenhar
-from matplotlib import pyplot as plt
-import matplotlib.patches as patches
+# from matplotlib import pyplot as plt
+# import matplotlib.patches as patches
 
 distancias = []
 
@@ -30,10 +30,43 @@ for row in splited_lines:
 
 del coordenadas[0]
 
+coordenadas.append(coordenadas[0]) # duplicando o primeiro valor para o final
+coordenadas[0].append(0) # adicionando 0 no final da primeira coordenada
+
 dis = distancia(coordenadas[0], coordenadas[1])
 
 n = len(coordenadas) # número de cidades (nós)
 
-begin('CV')
+begin('PCV')
 
 x = var('x', iprod(range(n), range(n)), bool)
+u = var('u', n)
+w = var('w', n)
+
+# função objetivo
+minimize(sum(w[i] for i in range(1,n-1)))
+
+# restrições
+
+# primeira restrição
+for i in range(n-1):
+    sum(x[i,j] for j in range(1,n) if i != j) == 1
+
+# segunda restrição
+for j in range(1,n):
+    sum(x[i,j] for i in range(n -1) if i != j) == 1
+
+# eliminação de sub-rotas
+for i in range(n-1):
+  for j in range(1,n):
+    if i!=j:
+      u[j] >= u[i] - M * (1 - x[i,j]) + coordenadas[i][2] + distancia(coordenadas[i], coordenadas[j])
+
+for i in range(1,n-1):
+    w[i] >= u[i] - coordenadas[i][3]
+
+# parametros de resolução
+solver(int, tm_lim=3600*1000)
+solver(int, gmi_cuts=1)
+
+solve()
